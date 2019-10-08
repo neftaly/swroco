c1d5fad8ce2f4006f9f51777ed7f8cc0e6c32914
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2019-10-08 23:36:00 +1300

    Update text

--- swroco.ino
+++ swroco.ino
@@ -6,6 +6,8 @@
 #include <DallasTemperature.h>
 
 #define VREF 5.0
+
+// Stat. computation
 #define SAMPLES 10
 QuickStats stats;
 unsigned int sampleIndex;
@@ -30,7 +32,6 @@ int tempDelay = 750 / (1 << (12 - tempResolution));
 float tempValue = 25;
 
 // Pressure
-// 
 #define PRESS_ALARM 800
 #define PRESS_VMIN 0.88 // 0.004A x 220R = 0.88V
 #define PRESS_VMAX 4.4 // 0.02A × 220R = 4.4V
@@ -39,11 +40,13 @@ float tempValue = 25;
 float pressValue, pressValues[SAMPLES];
 
 // Flow meters
+// In
 #define FLOW_0_INTERRUPT 0 // 0 = digital pin 2
-#define FLOW_1_INTERRUPT 1 // 1 = digital pin 3
 #define FLOW_0_PIN 2
-#define FLOW_1_PIN 3
 #define FLOW_0_Q 4.5 // f=4.5*Q; Q=L/min; f=hZ
+// Out
+#define FLOW_1_INTERRUPT 1 // 1 = digital pin 3
+#define FLOW_1_PIN 3
 #define FLOW_1_Q 55 // f=55*q
 #define FLOW_FREQ 0.5 // Seconds per update
 unsigned long flowTime;
@@ -60,23 +63,23 @@ void updateScreen() {
   oled.setCursor(0, 0);
 
   // Inflow
-  sprintf(buffer, "In:%4d.%02d L/min", (int)flowRate0, (int)(flowRate0*100)%100);
+  sprintf(buffer, "In:%4d.%02d l/min", (int)flowRate0, (int)(flowRate0*100)%100);
   oled.println(buffer);
 
   // Outflow
-  sprintf(buffer, "Out:%3d.%02d L/min", (int)flowRate1, (int)(flowRate1*100)%100);
+  sprintf(buffer, "Out:%3d.%02d l/min", (int)flowRate1, (int)(flowRate1*100)%100);
   oled.println(buffer);
 
   // TDS
   if (tempValue < 0) sprintf(buffer, "TDS:*TEMP ERROR*");
   else if (tdsValue <= 0) sprintf(buffer, "TDS:     *ERROR*");
-  else sprintf(buffer, "TDS:%8d PPM", (int)tdsValue);
+  else sprintf(buffer, "TDS:%8d ppm", (int)tdsValue);
   oled.println(buffer);
 
   // Pressure
-  // if (pressValue < 0) sprintf(buffer, "Press:   *ERROR*");
-  if (false) return;
-  else sprintf(buffer, "Press:%6d PSI", (int)pressValue);
+  // if (pressValue < 0) sprintf(buffer, "Press:   *ERROR*"); // Beware analogue drift
+  if (false) return; // I got 4-20ma not TTL by accident
+  else sprintf(buffer, "Press:%6d PSI", (int)pressValue); // Can't visualise non-ogre p unit
   oled.println(buffer);
 
   // Pressure alarm
@@ -84,7 +87,7 @@ void updateScreen() {
     oled.invertDisplay(true);
     oled.setInvertMode(true);
     oled.setCursor(1.5*8, 3);
-    oled.println(" PRESS ALARM ");
+    oled.println(" PRES. ALARM ");
     oled.setInvertMode(false);
     delay(100);
     oled.invertDisplay(false);
@@ -112,12 +115,12 @@ void updateTds() {
     133.42 * pow(compensationVoltage, 3)
     - 255.86 * pow(compensationVoltage, 2)
     + 857.39 * compensationVoltage
-  ) / 2;
+  ) / 2; // Presenting: some nice formula's severed torso
   tdsValue = stats.median(tdsValues, SAMPLES);
 }
 
 void updatePressure() {
-  float voltage = (float)analogRead(PRESS_PIN) * VREF / 1023.0;
+  float voltage = (float)analogRead(PRESS_PIN) * VREF / 1023.0; // I bet there's a stdlib somewhere
   pressValues[sampleIndex] = PRESS_SENSOR_MAX
    * (voltage - PRESS_VMIN)
    / (PRESS_VMAX - PRESS_VMIN);
@@ -170,5 +173,5 @@ void loop() {
 
   // Update sample counter
   sampleIndex++;
-  if (sampleIndex == SAMPLES) sampleIndex = 0;
+  if (sampleIndex == SAMPLES) sampleIndex = 0; // I don't do >= because I like living on the edge
 }
